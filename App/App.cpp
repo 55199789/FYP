3742ff90b3fdc16e843cac7b4bdf81fff65403af
55199789 <leqizheng2-c@my.cityu.edu.hk>
2021-03-15 16:58:17 +0800

    remove old files

--- App/App.cpp
+++ App/App.cpp
@@ -10,11 +10,7 @@
 #include "App.h"
 #include "Enclave_u.h"
 
-// add by ice
-#include <Eigen/Dense>
 #include <thread>
-#include <Eigen/Core>
-#include "scData.h"
 
 #include "threads_conf.h"
 
@@ -197,102 +193,6 @@ void threads_finish() {
 
     for (int i = 1; i < THREAD_NUM; i++)
         threads[i].join();
-}
-
-void ocall_toFile_plain(const char *filename, uint16_t *types, size_t types_len) {
-
-    printf("ocall_toFile......\n");
-
-    if(freopen(filename, "w", stdout)==NULL) {
-        printf("File %s open failed", filename);
-        return;
-    }
-    for(int i=0 ; i < (int)types_len - 1; i++)
-        printf("%d,", types[i]);
-    printf("%d\n", types[types_len-1]);
-    fclose(stdout);
-
-    printf("ocall_toFile end......\n");
-} 
-
-//ICE
-#include <sys/stat.h>
-char* file_input2(const char * FileName)
-{
-    FILE *pFile;
-    if((pFile = fopen(FileName, "rb"))==NULL) {
-        printf("\033[33m Open file %s failed. \033[0m\n", FileName);
-        exit(-1);
-    }
-
-    struct stat file_sate;
-    fstat(fileno(pFile), &file_sate);
-    size_t file_size = file_sate.st_size;
-    printf("%s: File Size: %u\n", __FUNCTION__, file_size);
-    char *buf = new char[file_size + 10];
-    size_t len = fread(buf, 1, file_size, pFile);
-    fclose(pFile);
-    printf("%u bytes are readed from file...\n", len);
-    buf[len] = 0;
-
-    return buf;
-}
-
-
-
-//ICE
-void eval_plain(int argc, char *argv[])
-{
-    if (argc != 3) {
-        printf("\033[33m input arguments for rows and cols is not specified! \033[0m\n");
-        printf("\033[33m usage: ./app row_val col_val \033[0m");
-        printf("\033[33m note: row_val means the counts for non-zero rows \033[0m\n");
-        exit(-1);
-    }
-
-    size_t input_rows = atoi(argv[1]);
-    size_t input_cols = atoi(argv[2]);
-
-    printf("Loading Data\n");
-
-    // char *buf = file_input2("../../GSE131907_raw_UMI_matrix_3kcell.csv");
-    char *buf = file_input2("../../3000-1.csv");
-printf("Here\n");
-    if (buf == NULL) {
-        printf("file_input fail...\n");
-        exit(1);
-    }
-    else {           
-        printf("file_input success...\n");
-    }
-    
-    // ecall_input(global_eid, buf);
-    // input(data, "./input/GSE131907_raw_UMI_matrix_3kcell.csv");
-    // printf("Size of data: %fMB\n", sizeof(matrix_real)*data.size()*1.0/1024/1024);
-    
-    ecall_eval_plain(global_eid, buf, input_rows, input_cols);
-    printf("ecall eval_plain returns...\n");
-}
-
-
-//ICE
-void eval_cipher(int argc, char *argv[])
-{
-    printf("Loading Data\n");
-
-    char *buf = file_input2("./input.enc");
-    if (buf == NULL) {
-        printf("file_input fail...\n");
-        exit(1);
-    }
-    else {           
-        printf("file_input success...\n");
-    }
-    
-    ecall_eval_cipher(global_eid, buf);
-    
-    // ecall_eval_plain(global_eid, buf, input_rows, input_cols);
-    printf("ecall_eval_cipher returns...\n");
 }
 
 /* Application entry */
--- Include/cluster/approx.h
+++
@@ -1,9 +0,0 @@
-#ifndef APPROX_ICE
-#define APPROX_ICE
-
-#include <string.h>
-
-void approx(double *x, double *y, size_t len,  \
-            double *xout, double * yout, size_t out_len);
-
-#endif
